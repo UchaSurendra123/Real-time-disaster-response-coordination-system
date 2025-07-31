09d8cc855c4935f9bfb0fdcd8989c8c611d4ee38
Surendra Ucha <130224051+UchaSurendra123@users.noreply.github.com>
2025-07-31 16:45:11 +0530

    Create disaster_responseurls.py

---
+++ disaster_responseurls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('incidents.urls')),
+    path('api/', include('resources.urls')),
+    path('api/', include('alerts.urls')),
+]
